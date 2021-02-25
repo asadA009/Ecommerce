b19e646377d3db4cddf1cd22dc1a0887614d135a
asadA009 <asadali4345425@gmail.com>
2021-02-25 22:35:17 +0500

    third

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -42,8 +42,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
-    'whitenoise.runserver_nostatic',
-    'django.contrib.staticfiles',
 ]
 
 MIDDLEWARE = [
